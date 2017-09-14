1428dbacc4cfc1a69366a5e83c3207b977ad3f24
Huy Dang <hqdang97@gmail.com>
2017-09-13 20:36:32 -0600

    fixed

--- Kyu 7/NiceArray.js
+++ Kyu 7/NiceArray.js
@@ -8,13 +8,20 @@ arr1 = [2,10,9,3] // return TRUE
 arr2 = [3,4,5,7] // return FALSE
 
 function isNice(arr){
+    if(arr.length === 0){
+        return false
+    }
+
+    
   const result = arr.map((item,index,arr) => {
       option1 = item + 1;
       option2 = item - 1;
-
-      if(arr.includes(option1) || arr.includes(option2)){
+    
+      if(arr.includes(option1)){
           return true
-      } else { return false }
+      } 
+      else if (arr.includes(option2)) { return true }
+      else { return false }
   })
   let count = 0;
 
@@ -34,4 +41,23 @@ function isNice(arr){
   }
 }
 
-console.log(isNice(arr2))
+console.log(isNice([ -2, 5, 0, 5, 12 ]))
+
+
+//THE BEST SOLUTION 
+
+function isNice(arr){
+  return !!arr.length && arr.every(x => arr.some(y => y === x - 1 ||  y === x + 1));
+}
+
+
+
+//BEST PRACTICE
+function isNice(arr){
+    var counter = 0
+    for(var i = 0; i < arr.length; i++) {
+        if (arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)) counter ++
+    }
+    if (counter == arr.length && arr.length != 0) return true
+    return false
+}
