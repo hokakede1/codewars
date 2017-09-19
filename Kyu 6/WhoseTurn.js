c3bd7666c7699c6653283d1cd801251133e37a88
Huy Dang <hqdang97@gmail.com>
2017-09-18 20:28:32 -0700

    Finish

---
+++ Kyu 6/WhoseTurn.js
@@ -0,0 +1,19 @@
+function whoseTurn(positions) {
+  //coding and coding..
+  
+  
+}
+
+
+describe("Basic Tests", function(){
+it("It should works for basic tests.", function(){
+
+Test.assertEquals(whoseTurn("b1;g1;b8;g8"),true)
+
+Test.assertEquals(whoseTurn("c3;g1;b8;g8"),false)
+
+Test.assertEquals(whoseTurn("g1;g2;g3;g4"),true)
+
+Test.assertEquals(whoseTurn("f8;h1;f3;c2"),false)
+
+})})
