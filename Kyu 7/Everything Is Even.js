6fadc638674ee6c983eeca3e93597a06f8cadfcc
Huy Dang <hqdang97@gmail.com>
2017-09-21 23:45:34 -0700

    setup

---
+++ Kyu 7/Everything Is Even.js
@@ -0,0 +1,25 @@
+// Here at Conformity Inc. we only like even things. We need you to put a stop to anything that does not conform to our strict even only policy.
+
+// Write a function that will convert any number to its nearest even number. When the number is halfway between two even numbers it should round away from zero.
+
+// Examples
+
+// 1 => 2
+
+// 0.5 => 0
+
+// -1 => -2
+
+// -0.5 => 0
+
+
+
+function ensureEven(n) {
+  // Code on squire...
+  
+}
+
+console.log(ensureEven(1)) // 2
+console.log(ensureEven(0.5)) // 0
+console.log(ensureEven(-1)) // -2
+console.log(ensureEven(-0.5)) // 0
