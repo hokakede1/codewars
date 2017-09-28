9d16972022277c9069e334b2bd8aae5c98628948
Huy Dang <hqdang97@gmail.com>
2017-09-27 22:15:01 -0600

    finish

--- Kyu 5/Sum of Pairs.js
+++ Kyu 5/Sum of Pairs.js
@@ -4,36 +4,60 @@
 
 //MY SOLUTION
 
-var sum_pairs=function(ints, s){
-    var result = [];
-    var time = 0;
-    //Going to create 2 nest loops for 1 number looking for a pair
-    ints.forEach((currentNum,index1,array) => {
-        // Slice out the current number
-        var testArray = array.slice(index1 + 1)
-        // Loop throu the rest of the array
-        testArray.forEach((item) => {
-            // if there is a pair push them into result
-            // check if it is the first time
-            var max = 0;
-            if(currentNum + item === s){
-                if(ints.indexOf(item) > max){
-                    max = ints.indexOf(item)
-                    result = ([currentNum, item])
-                }
-            }
-        })
-    })
+// var sum_pairs=function(ints, s){
+//     var result = [];
+//     var time = 0;
+//     //Going to create 2 nest loops for 1 number looking for a pair
+//     ints.forEach((currentNum,index1,array) => {
+//         // Slice out the current number
+//         var testArray = array.slice(index1 + 1)
+//         // Loop throu the rest of the array
+
+//         testArray.forEach((item,index) => {
+//             console.log(`at ${currentNum} has ${item}`)
+//             // if there is a pair push them into result
+//             // check if it is the first time
+            
+//             if(currentNum + item === s){
+//                     console.log([currentNum, item])
+//                     result.push([currentNum, item])
+                
+//             }
+//         })
+//     })
     
-    //Check for result, if nothing return undefined
-    return result.length === 0 ? undefined : result
+//     //Check for result, if nothing return undefined
+//     return result.length === 0 ? undefined : result[0]
+// }
+
+
+
+
+
+
+
+
+
+//FIX SOLUTION : UNDERSTAND THIS
+  var sum_pairs=function(ints, s){
+  var mem = {}
+  for (var i = 0; i < ints.length; ++i) {
+     console.log(mem)
+    if (mem[s - ints[i]]) {
+        console.log(i)
+        return [s - ints[i], ints[i]]
+    };
+    
+    mem[ints[i]] = true
+  }
 }
 
 
 
 
- console.log(sum_pairs([1, 4, 8, 7, 3, 15], 8));//[1, 7]
- console.log(sum_pairs([5, 9, 13, -3], 10)); //[13, -3]
-console.log(sum_pairs([10,5,2,3,7,5], 10)); //[3, 7]
+console.log(sum_pairs([1, 4, 8, 7, 3, 15], 8));//[1, 7]
+//  console.log(sum_pairs([5, 9, 13, -3], 10)); //[13, -3]
+//  console.log(sum_pairs([10,5,2,3,7,5], 10)); //[3, 7]
 
-
+//  console.log(sum_pairs([1,2,3,4,1,0], 2));
+//  console.log(sum_pairs([4,-2,3,3,4], 8));
