14fada8c928bbfc2a6611de9ba45a9dba2fe2421
Huy Dang <hqdang97@gmail.com>
2017-09-29 19:32:39 -0600

    a side note of better solution

--- Kyu 6/Multiples of 3 and 5.js
+++ Kyu 6/Multiples of 3 and 5.js
@@ -4,37 +4,37 @@
 
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
-function solution(number){
-console.log(number)
-  var arr = []
-  for(i=0; i < number; i++){
-      if(i % 3 === 0){
-          arr.push(i)
-      } else if (i % 5 === 0){
-        arr.push(i)
-      }
-  }
-  if(arr.length === 0){
-  return 0
-  } else {
-  return arr.reduce((a,b) => a+b)
-  }
+function solution(number) {
+    console.log(number)
+    var arr = []
+    for (i = 0; i < number; i++) {
+        if (i % 3 === 0) {
+            arr.push(i)
+        } else if (i % 5 === 0) {
+            arr.push(i)
+        }
+    }
+    if (arr.length === 0) {
+        return 0
+    } else {
+        return arr.reduce((a, b) => a + b)
+    }
 }
-
 
 console.log(solution(10)) //23
 
 
 
 
+
 // Better and simpler solution: 
-function solution(number){
-  var sum = 0;
-  
-  for(var i = 1;i< number; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum += i
+function solution(number) {
+    var sum = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
     }
-  }
-  return sum;
+    return sum;
 }
