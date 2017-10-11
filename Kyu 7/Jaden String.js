9a100acb3caee9b144b1aaac28aec27abda90d94
Huy Dang <hqdang97@gmail.com>
2017-10-11 10:28:20 -0600

    Finish

---
+++ Kyu 7/Jaden String.js
@@ -0,0 +1,11 @@
+
+
+
+String.prototype.toJadenCase = function () { 
+  return this.split(' ').map(item => item.charAt(0).toUpperCase() + item.slice(1)).join(' ');
+};
+
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase())
+// , "How Can Mirrors Be Real If Our Eyes Aren't Real"
