0ea303fc6b5e372e987f502d6e4215b99f3a4c4b
Huy Dang <hqdang97@gmail.com>
2017-10-19 14:21:18 -0600

    finish

--- Kyu 5/Valid Parentheses.js
+++ Kyu 5/Valid Parentheses.js
@@ -7,8 +7,10 @@
 function validParentheses(parens){
   // Split every char in the string into item in array
   var arr = parens.split('');
+  var first = arr.filter(item => item === '(').length === arr.filter(item => item === ')').length;
   // Seperate all item with "(" and ")" and compare the number if its the same
-  return arr.filter(item => item === '(').length === arr.filter(item => item === ')').length
+  var second = parens.charAt(0) === '(' && parens.charAt(arr.length - 1) === ')'
+  return first && second
 }
 
 
@@ -16,4 +18,4 @@ function validParentheses(parens){
 
   
 console.log(validParentheses( "()" )); //true
-console.log(validParentheses( "())" )); //false
+console.log(validParentheses( ")()()()(" )); //false
